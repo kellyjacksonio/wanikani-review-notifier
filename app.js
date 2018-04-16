2f902764dbcfbe2591ad8938467133de7c48c7a5
Kelly Jackson <kellyjackson.2015@gmail.com>
2018-04-16 01:22:51 +0000

    Add whether to send notification logic

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-var request = require('request');
+var request     = require('request'),
+    mongoose    = require('mongoose'),
+    Review      = require('./models/review'),
+    config      = require('./config.js');
 
+// database - mongodb://<dbuser>:<dbpassword>@ds135800.mlab.com:35800/wanikani-review-notifier
 // my API - https://www.wanikani.com/api/user/dafbfe09d39c7662e5274524b0c05604/study-queue
 // geena's API - https://www.wanikani.com/api/user/3dbf079d24812d364c5f2390ce9db4ca/study-queue
 // If on vacation mode, next_review_date === NULL
-// If you do a review, next_review_date updates to now making timeUntilReview === 0. Notifications should only be sent when timeUntilReview > 0.
+
+mongoose.connect(`mongodb://${config.dbUser}:${config.dbPassword}@ds135800.mlab.com:35800/wanikani-review-notifier`);
 
 var requestLoop = setInterval(function(){
     request('https://www.wanikani.com/api/user/dafbfe09d39c7662e5274524b0c05604/study-queue', (err, res, body) => {
@@ -13,25 +18,43 @@ var requestLoop = setInterval(function(){
         var numberOfReviews = parsedBody.requested_information.reviews_available;
         var now = Math.floor(Date.now() / 1000); // changes to seconds
         var timeUntilReview = nextReviewDate - now;
-        var notify = false;
         if(err) {
             console.log(err);
         } else {
-            console.log('Your username:', username);
-            console.log('Next review date:', nextReviewDate);
-            console.log('Now:', now);
+            //console.log('Your username:', username);
+            //console.log('Next review date:', nextReviewDate);
+            //console.log('Now:', now);
             console.log('Minutes until review:', timeUntilReview / 60); // if negative then a review is ready
-            console.log('Number of reviews:', numberOfReviews);
-            if(numberOfReviews > 0 && timeUntilReview === 0) {
-                notify = true;
-            } else if (timeUntilReview > 0) {
-                notify = false;
-            }
-            if(notify === true) {
-                console.log('send notification');
-                // send notification
-                notify = false;
-            }
+            // console.log('Number of reviews:', numberOfReviews);
+            
+            // determining whether to send notification logic
+            Review.find(function(err, dbReview) {
+                var storedReview = dbReview[0].numberOfReviews;
+                console.log('Stored review:', storedReview);
+                console.log('Current # of reviews', numberOfReviews);
+                if(err) return console.error(err); // error handling
+                if(storedReview === undefined || storedReview >= numberOfReviews) {
+                    console.log('dont send notification');
+                } else if(storedReview < numberOfReviews) {
+                    console.log('send notification');
+                    console.log(`you have ${numberOfReviews} number of reviews waiting for you!`);
+                } else {
+                    console.log('wtf happened');
+                }
+                
+                Review.remove({}, function(err) {
+                    if(err) {
+                        console.error(err);
+                    } else {
+                        console.log('cleared db');
+                    }
+                }); 
+                var review = new Review({numberOfReviews: numberOfReviews, now: now}); // create review object
+                review.save(function (err, storedReview) { // store numberOfReviews to review object
+                    if(err) return console.error(err);
+                    console.log('New stored review', storedReview);                    
+                });
+            });
         }
     });
 }, 10000);
