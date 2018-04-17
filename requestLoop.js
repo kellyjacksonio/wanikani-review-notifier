8d6d8db1b754d2b11385ceaff9e3c68bd3561abc
Kelly Jackson <kellyjackson.2015@gmail.com>
2018-04-17 22:16:56 +0000

    Add multiple user functionality

---
+++ requestLoop.js
@@ -0,0 +1,102 @@
+const   request     = require('request'),
+        mongoose    = require('mongoose'),
+        User        = require('./models/user'),
+        config      = require('./config.js'),
+        client      = require('twilio')(config.accountSid, config.authToken);
+
+// connect to database
+mongoose.connect(`mongodb://${config.dbUser}:${config.dbPassword}@ds135800.mlab.com:35800/wanikani-review-notifier`);
+
+User.find((err, user) => {
+    if(err) return console.error(err);
+    user.forEach((user) => {
+        if(user.receiveNotifications === true) {
+            requestLoop(user.username, user.phoneNumber, user.apiKey, user.storedReview);
+        }
+    });
+});
+
+
+function requestLoop(username, phoneNumber, apiKey, storedReview) {
+    setInterval(function(){
+        request(`https://www.wanikani.com/api/user/${apiKey}/study-queue`, (err, res, body) => {
+            var testStoredReview = storedReview;
+            var parsedBody = JSON.parse(body);
+            var WKusername = parsedBody.user_information.username;
+            var nextReviewDate = parsedBody.requested_information.next_review_date; // comes back in seconds
+            var numberOfReviews = parsedBody.requested_information.reviews_available;
+            var now = Math.floor(Date.now() / 1000); // changes to seconds
+            var timeUntilReview = nextReviewDate - now;
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('=====');
+                console.log('Your username:', WKusername);
+                console.log('Minutes until review:', Math.floor(timeUntilReview / 60));
+                
+                // determining whether to send notification logic
+                User.findOne({'username': username}, (err, user) => {
+                    console.log(`stored reviews for ${WKusername}: ${storedReview}`);
+                    console.log(`current # of reviews for ${WKusername}: ${numberOfReviews}`);
+                    
+                    if(err) return console.error(err); // error handling
+                    if(timeUntilReview === null) {
+                        console.log(`vacation mode is on for ${WKusername} - no notification`);
+                    } else if(storedReview === undefined || storedReview >= numberOfReviews) {
+                        console.log(`don't send notification to ${WKusername}`);
+                    } else if(storedReview < numberOfReviews) {
+                        console.log(`send notification to ${WKusername}`);
+                        client.messages
+                          .create({
+                            to: `+1${phoneNumber}`,
+                            from: config.twilioPhoneNumber,
+                            body: `${WKusername}, you have ${numberOfReviews} reviews waiting for you in wanikani! http://www.wanikani.com`,
+                          })
+                          .then(message => console.log(message.sid));
+                    } else {
+                        console.log(`notification error for ${username}`);
+                    }
+                    
+                    User.findOneAndUpdate(
+                        {'username': username}, 
+                        {$set:{'storedReview': numberOfReviews}}, 
+                        (err, updatedReview) => {
+                        if(err) return console.error(err);
+                    });
+                });
+                // Review.find(function(err, dbReview) {
+                //     var storedReview = dbReview[0].numberOfReviews;
+                //     console.log('Stored review:', storedReview);
+                //     console.log('Current # of reviews', numberOfReviews);
+                //     if(err) return console.error(err); // error handling
+                //     if(timeUntilReview === null) {
+                //         console.log('vacation mode is on - no notifications');
+                //     } else if(storedReview === undefined || storedReview >= numberOfReviews) {
+                //         console.log('dont send notification');
+                //     } else if(storedReview < numberOfReviews) {
+                //         console.log('send notification');
+                //         client.messages
+                //           .create({
+                //             to: `+1${phoneNumber}`,
+                //             from: config.twilioPhoneNumber,
+                //             body: `${WKusername}, you have ${numberOfReviews} reviews waiting for you in wanikani! http://www.wanikani.com`,
+                //           })
+                //           .then(message => console.log(message.sid));
+                //     } else {
+                //         console.log('wtf happened');
+                //     } 
+                    // Review.remove({}, function(err) {
+                    //     if(err) return console.error(err);
+                    // });
+                    
+                    // var review = new Review({numberOfReviews: numberOfReviews, now: now});
+                    // review.save(function (err, storedReview) { // store numberOfReviews to review object
+                    //     if(err) return console.error(err);                    
+                    // });
+                // });
+            }
+        });
+    }, 1000); // for production, change this to 60000
+}
+
+exports.module = requestLoop;
