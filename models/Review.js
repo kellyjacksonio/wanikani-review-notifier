b7e300bb384687cef4c611de0a00f741b4f1feb4
Kelly Jackson <kellyjackson.2015@gmail.com>
2018-04-17 22:14:36 +0000

    Merge review model with user

--- models/Review.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var reviewSchema = new Schema({
-    numberOfReviews: Number,
-    now: Number
-});
-
-module.exports = mongoose.model('Review', reviewSchema);
