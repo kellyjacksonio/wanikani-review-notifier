84c64985cec8fc28483c229d6b5366c0d8ad280c
Kelly Jackson <kellyjackson.2015@gmail.com>
2018-04-18 00:35:04 +0000

    Delete file

--- models/review.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var reviewSchema = new Schema({
-    numberOfReviews: Number
-});
-
-module.exports = mongoose.model('Review', reviewSchema);
