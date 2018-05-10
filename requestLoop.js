c75739279e51279c954a09f24480fd7310e8d649
Kelly Jackson <kellyjackson.2015@gmail.com>
2018-05-10 14:51:21 +0000

    Fix formatting

--- requestLoop.js
+++ requestLoop.js
@@ -30,71 +30,71 @@ var requestLoop = setInterval(function(){
 
 
 function sendNotification(username, phoneNumber, apiKey, storedReview, receiveEmail, receiveText) {
-        request(`https://www.wanikani.com/api/user/${apiKey}/study-queue`, (err, res, body) => {
-            var testStoredReview = storedReview;
-            var parsedBody = JSON.parse(body);
-            var WKusername = parsedBody.user_information.username;
-            var nextReviewDate = parsedBody.requested_information.next_review_date; // comes back in seconds
-            var numberOfReviews = parsedBody.requested_information.reviews_available;
-            var now = Math.floor(Date.now() / 1000); // changes to seconds
-            var timeUntilReview = nextReviewDate - now;
-            if(err) {
-                console.log(err);
-            } else {
-                console.log('=====');
-                console.log('Your username:', WKusername);
-                console.log('Minutes until review:', Math.floor(timeUntilReview / 60));
+    request(`https://www.wanikani.com/api/user/${apiKey}/study-queue`, (err, res, body) => {
+        var testStoredReview = storedReview;
+        var parsedBody = JSON.parse(body);
+        var WKusername = parsedBody.user_information.username;
+        var nextReviewDate = parsedBody.requested_information.next_review_date; // comes back in seconds
+        var numberOfReviews = parsedBody.requested_information.reviews_available;
+        var now = Math.floor(Date.now() / 1000); // changes to seconds
+        var timeUntilReview = nextReviewDate - now;
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('=====');
+            console.log('Your username:', WKusername);
+            console.log('Minutes until review:', Math.floor(timeUntilReview / 60));
+            
+            // determining whether to send notification logic
+            User.findOne({'username': username}, (err, user) => {
+                console.log(`stored reviews for ${WKusername}: ${storedReview}`);
+                console.log(`current # of reviews for ${WKusername}: ${numberOfReviews}`);
                 
-                // determining whether to send notification logic
-                User.findOne({'username': username}, (err, user) => {
-                    console.log(`stored reviews for ${WKusername}: ${storedReview}`);
-                    console.log(`current # of reviews for ${WKusername}: ${numberOfReviews}`);
-                    
-                    if(err) return console.error(err); // error handling
-                    if(timeUntilReview === null) {
-                        console.log(`vacation mode is on for ${WKusername} - no notification`);
-                    } else if(storedReview === undefined || storedReview >= numberOfReviews) {
-                        console.log(`don't send notification to ${WKusername}`);
-                    } else if(storedReview < numberOfReviews) {
-                        console.log(`send notification to ${WKusername}`);
-                        if(receiveText) {
-                            client.messages
-                              .create({
+                if(err) return console.error(err); // error handling
+                if(timeUntilReview === null) {
+                    console.log(`vacation mode is on for ${WKusername} - no notification`);
+                } else if(storedReview === undefined || storedReview >= numberOfReviews) {
+                    console.log(`don't send notification to ${WKusername}`);
+                } else if(storedReview < numberOfReviews) {
+                    console.log(`send notification to ${WKusername}`);
+                    if(receiveText) {
+                        client.messages
+                            .create({
                                 to: `+1${phoneNumber}`,
                                 from: config.twilioPhoneNumber,
                                 body: `${WKusername}, you have ${numberOfReviews} reviews waiting for you in wanikani! http://www.wanikani.com`,
-                              })
-                              .then(message => console.log(`text sent to ${WKusername}`));
+                            })
+                            .then(message => console.log(`text sent to ${WKusername}`));
                         }
                         
-                        if(receiveEmail) {
-                            var person = {
-                                username: `${WKusername}`,
-                                numberOfReviews: `${numberOfReviews}`,
-                                email: `${username}`,
-                                subject: `${numberOfReviews} New Reviews!`,
-                            };
-                            
-                            email('test', person, (err, result) => {
-                                if(err) return console.log(err);
-                                console.log(`email send to ${WKusername}`);
-                            });
-                        }
-                    } else {
-                        console.log(`notification error for ${username}`);
+                    if(receiveEmail) {
+                        var person = {
+                            username: `${WKusername}`,
+                            numberOfReviews: `${numberOfReviews}`,
+                            email: `${username}`,
+                            subject: `${numberOfReviews} New Reviews!`,
+                        };
+                        
+                        email('test', person, (err, result) => {
+                            if(err) return console.log(err);
+                            console.log(`email send to ${WKusername}`);
+                        });
                     }
-                    
-                    User.findOneAndUpdate(
-                        {username: username}, 
-                        {$set:{storedReview: numberOfReviews}},
-                        {returnNewDocument: true},
-                        (err, updatedReview) => {
-                        if(err) return console.error(err);
-                        console.log(updatedReview)
-                    });
+                } else {
+                    console.log(`notification error for ${username}`);
+                }
+                
+                User.findOneAndUpdate(
+                    {username: username}, 
+                    {$set:{storedReview: numberOfReviews}},
+                    {returnNewDocument: true},
+                    (err, updatedReview) => {
+                    if(err) return console.error(err);
+                    console.log(updatedReview)
                 });
-            }
-        });
+            });
+        }
+    });
 }
 
 exports.module = requestLoop;
